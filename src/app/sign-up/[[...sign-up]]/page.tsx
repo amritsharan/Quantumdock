88b2d5a0c1c8f1e6bf49575ca69f5eedb041ed6c
1SI23CI005 AMRIT S R <1si23ci005@sit.ac.in>
2025-10-26 01:13:18 +0000

    when it comes to sign up ui is not good. please revert signup ui to the

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,19 @@
+
 import { SignUp } from "@clerk/nextjs";
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 
 export default function Page() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <SignUp />
+       <Card className="w-full max-w-md">
+        <CardHeader className="text-center">
+          <CardTitle>Create an account</CardTitle>
+          <CardDescription>to continue to QuantumDock</CardDescription>
+        </CardHeader>
+        <CardContent className="flex justify-center">
+          <SignUp routing="hash" />
+        </CardContent>
+      </Card>
     </div>
   );
 }
