f7618093a4ee72b4fe4958c72dd66a5a78306779
1SI23CI005 AMRIT S R <1si23ci005@sit.ac.in>
2025-10-26 01:15:29 +0000

    no remove that white backgorund for this just keep it plain

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,19 +1,10 @@
 
 import { SignUp } from "@clerk/nextjs";
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
 
 export default function Page() {
   return (
     <div className="flex justify-center items-center h-screen">
-       <Card className="w-full max-w-md">
-        <CardHeader className="text-center">
-          <CardTitle>Create an account</CardTitle>
-          <CardDescription>to continue to QuantumDock</CardDescription>
-        </CardHeader>
-        <CardContent className="flex justify-center">
-          <SignUp routing="hash" />
-        </CardContent>
-      </Card>
+      <SignUp routing="hash" />
     </div>
   );
 }
