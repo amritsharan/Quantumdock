c70e7d45e71972d6d8df699f521a129361c498b2
1SI23CI005 AMRIT S R <1si23ci005@sit.ac.in>
2025-10-26 02:20:15 +0000

    🧠 Step 5: Initialize Realtime Database in Your Code
    In your JavaScript

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,10 @@
+
 export const firebaseConfig = {
   "projectId": "studio-9727664232-3c95f",
   "appId": "1:166627752225:web:c9da438ae60c463f0a6660",
   "apiKey": "AIzaSyBqESQCbUJBLmQY_BDIemNOGhL-9RCR-aA",
   "authDomain": "studio-9727664232-3c95f.firebaseapp.com",
   "measurementId": "",
-  "messagingSenderId": "166627752225"
+  "messagingSenderId": "166627752225",
+  "databaseURL": process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL
 };
