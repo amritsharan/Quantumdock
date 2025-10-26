c601d1516c2bdca2e2b4b9ff0847438f30ac5446
1SI23CI005 AMRIT S R <1si23ci005@sit.ac.in>
2025-10-26 01:20:49 +0000

    remove this white background keep it the background which is outside of

--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -4,8 +4,13 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="flex justify-center items-center h-screen">
-      <SignUp />
+      <SignUp
+        appearance={{
+          elements: {
+            card: "bg-transparent shadow-none border-none",
+          },
+        }}
+      />
     </div>
   );
 }
-
