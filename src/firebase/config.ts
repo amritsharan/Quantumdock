ac604d644df3133310f4d780628bdfd3ed08af48
1SI23CI005 AMRIT S R <1si23ci005@sit.ac.in>
2025-10-30 12:55:24 +0000

    "AIzaSyBpWrLlkk0uct3qjY3HxpYH0vMqnm8yFtQ" this is the api key use this a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9727664232-3c95f",
   "appId": "1:166627752225:web:c9da438ae60c463f0a6660",
-  "apiKey": "AIzaSyBqESQCbUJBLmQY_BDIemNOGhL-9RCR-aA",
+  "apiKey": "AIzaSyBpWrLlkk0uct3qjY3HxpYH0vMqnm8yFtQ",
   "authDomain": "studio-9727664232-3c95f.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "166627752225",
